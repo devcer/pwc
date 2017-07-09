0c3cdea85e38bc94c5557fc9ecbb489d234d9079
viswanathamsantosh <viswanathamsantosh@gmail.com>
2017-07-09 17:50:35 +0530

    added json and populated table

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ app.controller('appController', function($scope, $http) {
 	$scope.showUriSection=false;
 	$scope.showUniqueId=false;
   $scope.showGenerateReport=true;
+  $http({
+  method: "GET",
+  url: "AadharoutPut.json",
+  dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.dashboardData=response.data;
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
 });
 app.directive("uri",function(){
   return {
