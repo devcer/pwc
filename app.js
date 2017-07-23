4418ca1231d5940e851e183fbc1fa999e3783aca
viswanathamsantosh <viswanathamsantosh@gmail.com>
2017-07-24 00:50:44 +0530

    made pre-final changes for demo

--- app.js
+++ app.js
@@ -7,48 +7,143 @@ app.service('GraphService',function () {
     function drawChart() {
       var graphData=[];
       for(var i=0;i<tableData.length;i++){
-        graphData.push([tableData[i].url,tableData[i].aadhars.length]);
+        graphData.push([tableData[i].url,tableData[i][tableData[i].type].length, tableData[i].color]);
       }
 
       var data = new google.visualization.DataTable();
       data.addColumn('string', 'url');
       data.addColumn('number', 'hits');
+      data.addColumn({type: 'string', role: 'style'});
       data.addRows(graphData);
       var options = {'title':'Data with urls and hits',
-                     'width':400,
-                     'height':300};
+                     'width':600,
+                     'height':400};
       var chart = new google.visualization.BarChart (document.getElementById('chart_div'));
-        chart.draw(data, options);
+      
+      function selectHandler(){
+        console.log("selected item:"+ chart.getSelection()[0]);
+        var selectedItem = chart.getSelection()[0];
+        if (selectedItem) {
+          var topping = data.getValue(selectedItem.row, 0);
+          window.open(topping);
+         }
+      }
+
+      google.visualization.events.addListener(chart, 'select', selectHandler);
+      chart.draw(data, options);
       }
     return "nothing";
   };
 });
+
 
 app.controller('appController', function($scope, $http, GraphService) {
 	$scope.showUriSection=false;
 	$scope.showUniqueId=false;
   $scope.showGenerateReport=true;
   $scope.users=0;
-
+  $scope.masterData = [];
   var dashboardDataLen=0;
+
+
   $http({
     method: "GET",
-    url: "AadharoutPut.json",
+    url: "var/www/html/outputs/GREEN/aadhar.json",
     dataType: "json"
   }).then(function successCallback(response) {
-    dashboardDataLen=response.data.length;
-    $scope.dashboardData=response.data;
-    for(var i=0;i<dashboardDataLen;i++){
-      $scope.users=$scope.users + response.data[i].aadhars.length;
-    }
-    $('#myTable').DataTable({
-      'searching': false,
-      'autoWidth': false
-    });
-    GraphService.drawGraph($scope.dashboardData);
+    $scope.pushToMasterJson(response.data, "green");
   }, function errorCallback(response) {
     console.log("Error:"+response)
   });
+
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/GREEN/pan.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "green");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/GREEN/license.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "green");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/RED/aadhar.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "red");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/RED/pan.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "red");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/RED/license.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "red");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/YELLOW/aadhar.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "yellow");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/YELLOW/pan.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "yellow");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+  $http({
+    method: "GET",
+    url: "var/www/html/outputs/YELLOW/license.json",
+    dataType: "json"
+  }).then(function successCallback(response) {
+    $scope.pushToMasterJson(response.data, "yellow");
+  }, function errorCallback(response) {
+    console.log("Error:"+response)
+  });
+
+  $scope.pushToMasterJson=(jsonFile, color)=>{
+    for(var i=0;i<jsonFile.length;i++){
+      jsonFile[i].type=Object.keys(jsonFile[i])[0];
+      jsonFile[i].color=color;
+      $scope.users=$scope.users+jsonFile[i][jsonFile[i].type].length;
+      $scope.masterData.push(jsonFile[i]);
+    }
+    dashboardDataLen+=1;
+    if(dashboardDataLen==9)
+      GraphService.drawGraph($scope.masterData);
+  }
 });
 app.controller("userSearchCtrl",function($scope){
   $scope.enableWebsiteUrlCorrect = false;
@@ -108,6 +203,11 @@ function handleFiles(files){
   $("#submit-upload-url").click();
 }
 
+$(document).ready( function () {
+    $('#myTable').DataTable({
+      'autoWidth': false
+    });
+} );
 // $("#website-url").on("keydown",(context)=>{
 //   context.target.value.startsWith("https://");
 //   console.log("ES6 works");
